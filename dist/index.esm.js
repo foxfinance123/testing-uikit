f6f734adea9393dcdf05d74b5cea5d955bcb3f4e
Kenny Yew <yoongyy@gmail.com>
2021-07-07 13:50:53 +0800

    Test

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2650,7 +2650,7 @@ var Menu = function (_a) {
     var account = _a.account, login = _a.login, logout = _a.logout, isDark = _a.isDark, toggleTheme = _a.toggleTheme, langs = _a.langs, setLang = _a.setLang, currentLang = _a.currentLang, cakePriceUsd = _a.cakePriceUsd, links = _a.links, priceLink = _a.priceLink, profile = _a.profile, children = _a.children;
     var isXl = useMatchBreakpoints().isXl;
     var isMobile = isXl === false;
-    var _c = useState(isMobile), isPushed = _c[0], setIsPushed = _c[1];
+    var _c = useState(!isMobile), isPushed = _c[0], setIsPushed = _c[1];
     var _d = useState(true), showMenu = _d[0], setShowMenu = _d[1];
     var refPrevOffset = useRef(window.pageYOffset);
     useEffect(function () {
