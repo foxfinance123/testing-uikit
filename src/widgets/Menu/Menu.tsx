2a0fdc9733599fbfb3f99aad0c7e98db0a8991eb
Kenny Yew <yoongyy@gmail.com>
2021-07-07 16:18:47 +0800

    Test

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -64,146 +64,146 @@ const MobileOnlyOverlay = styled(Overlay)`
 `;
 
 const Menu: React.FC<NavProps> = ({
-                                    account,
-                                    login,
-                                    logout,
-                                    isDark,
-                                    toggleTheme,
-                                    langs,
-                                    setLang,
-                                    currentLang,
-                                    cakePriceUsd,
-                                    links,
-                                    priceLink,
-                                    profile,
-                                    children,
+                                      account,
+                                      login,
+                                      logout,
+                                      isDark,
+                                      toggleTheme,
+                                      langs,
+                                      setLang,
+                                      currentLang,
+                                      cakePriceUsd,
+                                      links,
+                                      priceLink,
+                                      profile,
+                                      children,
                                   }) => {
-  const { isXl } = useMatchBreakpoints();
-  const isMobile = isXl === false;
-  const [isPushed, setIsPushed] = useState(!isMobile);
-  const [showMenu, setShowMenu] = useState(true);
-  const refPrevOffset = useRef(window.pageYOffset);
+    const { isXl } = useMatchBreakpoints();
+    const isMobile = isXl === false;
+    const [isPushed, setIsPushed] = useState(!isMobile);
+    const [showMenu, setShowMenu] = useState(true);
+    const refPrevOffset = useRef(window.pageYOffset);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentOffset = window.pageYOffset;
-      const isBottomOfPage = window.document.body.clientHeight === currentOffset + window.innerHeight;
-      const isTopOfPage = currentOffset === 0;
-      // Always show the menu when user reach the top
-      if (isTopOfPage) {
-        setShowMenu(true);
-      }
-      // Avoid triggering anything at the bottom because of layout shift
-      else if (!isBottomOfPage) {
-        if (currentOffset < refPrevOffset.current) {
-          // Has scroll up
-          setShowMenu(true);
-        } else {
-          // Has scroll down
-          setShowMenu(false);
-        }
-      }
-      refPrevOffset.current = currentOffset;
-    };
-    const throttledHandleScroll = throttle(handleScroll, 200);
-
-    window.addEventListener("scroll", throttledHandleScroll);
-    return () => {
-      window.removeEventListener("scroll", throttledHandleScroll);
-    };
-  }, []);
-
-  // Find the home link if provided
-  const homeLink = links.find((link) => link.label === "Home");
-
-  const Icons = (IconModule as unknown) as { [key: string]: React.FC<SvgProps> };
-
-  return (
-      <Wrapper>
-        <StyledNav showMenu={showMenu}>
-          <Logo
-              isMobile={isMobile}
-              isPushed={isPushed}
-              togglePush={() => setIsPushed((prevState: boolean) => !prevState)}
-              isDark={isDark}
-              href={homeLink?.href ?? "/"}
-          />
-
-          { !isMobile && links.map((entry:any) => {
-            const Icon = Icons[entry.icon];
-            const iconElement = <Icon width="24px" mr="8px" />;
-            const calloutClass = entry.calloutClass ? entry.calloutClass : undefined;
-
-            // const handleClick = isMobile ? () => setIsPushed(false) : undefined;
-
-            if (entry.items) {
-              return (
-                  <Dropdown key={entry.label} position="top" target={<Icon {...iconElement} />}>
-                    {entry.items.map((item:any) => (
-                        <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
-                          {item.label}
-                        </Link>
-                    ))}
-                  </Dropdown>
-                  // <Accordion2
-                  //     key={entry.label}
-                  //     isPushed={isPushed}
-                  //     pushNav={setIsPushed}
-                  //     icon={iconElement}
-                  //     label={entry.label}
-                  //     initialOpenState={entry.initialOpenState}
-                  //     className={calloutClass}
-                  // >
-                  //   {isPushed &&
-                  //   entry.items.map((item:any) => (
-                  //       <MenuEntry key={item.href} secondary isActive={item.href === location.pathname}>
-                  //         <MenuLink href={item.href}>{item.label}</MenuLink>
-                  //       </MenuEntry>
-                  //   ))}
-                  // </Accordion2>
-              );
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentOffset = window.pageYOffset;
+            const isBottomOfPage = window.document.body.clientHeight === currentOffset + window.innerHeight;
+            const isTopOfPage = currentOffset === 0;
+            // Always show the menu when user reach the top
+            if (isTopOfPage) {
+                setShowMenu(true);
             }
-            return (
-                <MenuEntry key={entry.label} isActive={entry.href === location.pathname} className={calloutClass}>
-                  <MenuLink href={entry.href}>
-                    {iconElement}
-                    <LinkLabel isPushed={isPushed}>{entry.label}</LinkLabel>
-                  </MenuLink>
-                </MenuEntry>
-            );
-          })}
+            // Avoid triggering anything at the bottom because of layout shift
+            else if (!isBottomOfPage) {
+                if (currentOffset < refPrevOffset.current) {
+                    // Has scroll up
+                    setShowMenu(true);
+                } else {
+                    // Has scroll down
+                    setShowMenu(false);
+                }
+            }
+            refPrevOffset.current = currentOffset;
+        };
+        const throttledHandleScroll = throttle(handleScroll, 200);
 
-          <Flex>
-            <UserBlock account={account} login={login} logout={logout} />
-            {profile && <Avatar profile={profile} />}
-          </Flex>
-        </StyledNav>
+        window.addEventListener("scroll", throttledHandleScroll);
+        return () => {
+            window.removeEventListener("scroll", throttledHandleScroll);
+        };
+    }, []);
 
-        <BodyWrapper>
-          {isMobile &&
-          <Panel
-              isPushed={isPushed}
-              isMobile={isMobile}
-              showMenu={showMenu}
-              isDark={isDark}
-              toggleTheme={toggleTheme}
-              langs={langs}
-              setLang={setLang}
-              currentLang={currentLang}
-              cakePriceUsd={cakePriceUsd}
-              pushNav={setIsPushed}
-              links={links}
-              priceLink={priceLink}
-          />
-          }
-          <Inner isPushed={isPushed} showMenu={showMenu}>
-            {children}
-          </Inner>
-          <MobileOnlyOverlay show={isPushed} onClick={() => setIsPushed(false)} role="presentation"/>
-        </BodyWrapper>
+    // Find the home link if provided
+    const homeLink = links.find((link) => link.label === "Home");
 
-      </Wrapper>
-  );
+    const Icons = (IconModule as unknown) as { [key: string]: React.FC<SvgProps> };
+
+    return (
+        <Wrapper>
+            <StyledNav showMenu={showMenu}>
+                <Logo
+                    isMobile={isMobile}
+                    isPushed={isPushed}
+                    togglePush={() => setIsPushed((prevState: boolean) => !prevState)}
+                    isDark={isDark}
+                    href={homeLink?.href ?? "/"}
+                />
+
+                { !isMobile && links.map((entry:any) => {
+                    const Icon = Icons[entry.icon];
+                    const iconElement = <Icon width="24px" mr="8px" />;
+                    const calloutClass = entry.calloutClass ? entry.calloutClass : undefined;
+
+                    // const handleClick = isMobile ? () => setIsPushed(false) : undefined;
+
+                    if (entry.items) {
+                        return (
+                            <Dropdown key={entry.label} position="bottom" target={<Icon {...iconElement} />}>
+                                {entry.items.map((item:any) => (
+                                    <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
+                                        {item.label}
+                                    </Link>
+                                ))}
+                            </Dropdown>
+                            // <Accordion2
+                            //     key={entry.label}
+                            //     isPushed={isPushed}
+                            //     pushNav={setIsPushed}
+                            //     icon={iconElement}
+                            //     label={entry.label}
+                            //     initialOpenState={entry.initialOpenState}
+                            //     className={calloutClass}
+                            // >
+                            //   {isPushed &&
+                            //   entry.items.map((item:any) => (
+                            //       <MenuEntry key={item.href} secondary isActive={item.href === location.pathname}>
+                            //         <MenuLink href={item.href}>{item.label}</MenuLink>
+                            //       </MenuEntry>
+                            //   ))}
+                            // </Accordion2>
+                        );
+                    }
+                    return (
+                        <MenuEntry key={entry.label} isActive={entry.href === location.pathname} className={calloutClass}>
+                            <MenuLink href={entry.href}>
+                                {iconElement}
+                                <LinkLabel isPushed={isPushed}>{entry.label}</LinkLabel>
+                            </MenuLink>
+                        </MenuEntry>
+                    );
+                })}
+
+                <Flex>
+                    <UserBlock account={account} login={login} logout={logout} />
+                    {profile && <Avatar profile={profile} />}
+                </Flex>
+            </StyledNav>
+
+            <BodyWrapper>
+                {isMobile &&
+                <Panel
+                    isPushed={isPushed}
+                    isMobile={isMobile}
+                    showMenu={showMenu}
+                    isDark={isDark}
+                    toggleTheme={toggleTheme}
+                    langs={langs}
+                    setLang={setLang}
+                    currentLang={currentLang}
+                    cakePriceUsd={cakePriceUsd}
+                    pushNav={setIsPushed}
+                    links={links}
+                    priceLink={priceLink}
+                />
+                }
+                <Inner isPushed={isPushed} showMenu={showMenu}>
+                    {children}
+                </Inner>
+                <MobileOnlyOverlay show={isPushed} onClick={() => setIsPushed(false)} role="presentation"/>
+            </BodyWrapper>
+
+        </Wrapper>
+    );
 };
 
 export default Menu;
